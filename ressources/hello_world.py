6837fdfc0584fdc90aa8077819617012d94a5535
Fuki <fukiwony@gmail.com>
2019-07-03 14:03:54 +0200

    Update hello_world.py

--- ressources/hello_world.py
+++ ressources/hello_world.py
@@ -3,10 +3,4 @@ from flask import after_this_request
 
 class HelloWorld(Resource):
     def get(self):
-
-        @after_this_request
-        def set_cookie(response):
-            response.set_cookie('user','Papa',max_age=64800,httponly=False)
-            return response
-
         return {'hello': 'world'}
